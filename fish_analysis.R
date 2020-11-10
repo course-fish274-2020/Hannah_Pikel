bfa666f38996b53f415e04e93d31501079230be5
hpikel <hannahpikel16@gmail.com>
2020-11-10 10:32:49 -0800

    add comment for the mutate code

--- fish_analysis.R
+++ fish_analysis.R
@@ -2,6 +2,7 @@
 
 fish_data <- read.csv("data/Gaeta_etal_CLC_data (1).csv")
 
+#creating an additional column that classifies fish bigger than 300mm as big
 library(dplyr)
 fish_data_cat <- fish_data %>%
-  mutate(length_cat = ifelse(length > 200, "big", "small"))
+  mutate(length_cat = ifelse(length > 300, "big", "small"))
