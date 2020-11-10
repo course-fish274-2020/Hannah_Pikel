92c55d5a46e00e0844b27e785699178cee946597
hpikel <hannahpikel16@gmail.com>
2020-11-10 11:09:53 -0800

    create histogram and save it to plots

--- fish_analysis.R
+++ fish_analysis.R
@@ -5,6 +5,7 @@ fish_data <- read.csv("data/Gaeta_etal_CLC_data_1.csv")
 #creating an additional column that classifies fish bigger than 300mm as big
 
 library(dplyr)
+library(ggplot2)
 fish_data_cat <- fish_data %>%
   mutate(length_cat = ifelse(length > 300, "big", "small")) 
 
@@ -16,3 +17,5 @@ fish_data_cat_clean <- filter(fish_data_cat, scalelength > 1)
 
 ggplot(fish_data_cat, aes(x = scalelength, fill = length_cat)) +
   geom_histogram()
+
+ggsave("plots/scale_hist_by_length.jpg")
